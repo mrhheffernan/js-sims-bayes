270f7305b62d2f1d9f13a9d34d8fd9415bad2b22
Jean-francois Paquet <jp401@grads-20.internal.phy.duke.edu>
2019-05-20 09:55:07 -0400

    Simple script to print the content of a single hydro event

---
+++ model_calculations/print_single_event.py
@@ -0,0 +1,36 @@
+##!/usr/bin/env python3
+#
+import sys
+import numpy as np
+from collections.abc import Iterable
+
+
+# Input data format
+from file_format_single_event import *
+
+filename=sys.argv[1]
+data = np.fromfile(filename, dtype=result_dtype)
+
+# Loop over data structure
+# Assumes that "data" is a numpy array with dtype given
+# by the array "structure" (though the latter is not a dtype object)
+def print_data_structure(data, structure):
+
+    n_items=len(structure)
+
+    if (n_items > 0):
+        for n, item in enumerate(structure):
+            tmp_struct=structure[n]
+            # If the item has substructure, recurse on it
+            if (not isinstance(tmp_struct[1], str)) and (isinstance(tmp_struct[1], Iterable)):
+                print(tmp_struct[0])
+                print_data_structure(data[tmp_struct[0]],tmp_struct[1])
+            # If no substructure, just output the result
+            else:
+                print(tmp_struct[0],data[tmp_struct[0]])
+
+print_data_structure(data, result_dtype)
+
+
+
+
