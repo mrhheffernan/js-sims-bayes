1c6be27a943320b962aeaa10ba2f1e2436b3f197
Jean-francois Paquet <jp401@grads-20.internal.phy.duke.edu>
2019-05-27 22:26:09 -0400

    Clean up and comment plotting script

--- model_calculations/plot_obs.py
+++ model_calculations/plot_obs.py
@@ -9,13 +9,21 @@ import re
 # Output data format
 from file_format_event_average import *
 
-# Observables are grouped in plots according to this regular expression
+
+#################################################################################
+#### Try to figure out semi-automatically what observables to group together ####
+#################################################################################
+
+# This is the input:
+# Specifies how observables are grouped according to these regular expression
 regex_obs_to_group_list=[
 (r'$\pi$/K/p dN/dy',"dN_dy_(pion|kaon|proton)"),
 (r'$\pi$/K/p $\langle p_T \rangle$',"mean_pT_(pion|kaon|proton)"),
 (r'$\Lambda/\Omega/\Xi$ dN/dy',"dN_dy_(Lambda|Omega|Xi)"),  
 (r'$v_n\{2\}$',"v[2-5+]2") 
 ]
+
+# This parts figures out how to group observables based on the regular expressions
 
 obs_to_group={}
 # Loop over observables to see which ones to group
@@ -50,16 +58,12 @@ for n, (key, value) in enumerate(obs_to_group.items()):
 
     final_obs_grouping.setdefault(newvalue, []).append(key)
 
-## Plot ID
-#plot_id=1
-#plot_id_dict={}
-#for  (key, value) in final_obs_grouping.items():
-#    for obs in value:
-#        plot_id_dict[obs]=plot_id
-#    plot_id+=1
-#
-#print(plot_id_dict)
 
+
+
+##############
+#### Plot ####
+##############
 
 def plot(calcs):
 
@@ -85,142 +89,24 @@ def plot(calcs):
             plt.errorbar(mid_centrality, mean_values, yerr=stat_uncert)
 
     plt.tight_layout(True)
-    plt.savefig("miaw.pdf")
+    #plt.savefig("obs.pdf")
     plt.show()
-
-
-#                info = calcs['Pb-Pb-2760']['dNch_deta']['mean'][:, idf]
-#
-#
-#
-#                # dNdeta
-#                info = calcs['Pb-Pb-2760']['dNch_deta']['mean'][:, idf]
-#                #info = calculate_dNdeta(res, 'ALICE', cenb, idf)
-#                #entry['Pb-Pb-2760']['dNch_deta'][:, idf] = info['obs']
-#                #if plot:
-#                plt.subplot(2,4,1)
-#                #plt.errorbar(info['cenM'], info['obs'], yerr=info['err'], fmt='ro-')
-#                plt.errorbar(info['cenM'], info['obs'], yerr=info['err'])
-#                plt.ylim(0,2000)
-#                plt.xlabel(r'Centrality (%)', fontsize=7)
-#                #plt.xlim(60,70)
-#                #plt.ylim(65,73)
-#                plt.ylabel(r'charged $dN/d\eta$', fontsize=7)
-
-#               # dETdeta
-#
-#               cenb = np.array([[0, 2.5], [2.5, 5], [5, 7.5], [7.5, 10], [10, 12.5], [12.5, 15], [15, 17.5], [17.5, 20], [20, 22.5], [22.5, 25], [25, 27.5], [27.5, 30], [30, 32.5], [32.5, 35], [35, 37.5], [37.5, 40], [40, 45], [45, 50], [50, 55], [55, 60], [60, 65], [65, 70]])
-#               info = calculate_dETdeta(res, 'ALICE', cenb, idf)
-#               entry['Pb-Pb-2760']['dET_deta'][:,idf] = info['obs']
-#               if plot:
-#                       plt.subplot(2,4,2)
-#                       #plt.errorbar(info['cenM'], info['obs'], yerr=info['err'], fmt='bo-')
-#                       plt.errorbar(info['cenM'], info['obs'], yerr=info['err'])
-#                       plt.ylim(0,2000)
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$dE_T/d\eta$', fontsize=7)
-#
-#               # dN(pid)/dy
-#               cenb = np.array([[0,5],[5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70]])
-#               info = calculate_dNdy(res, 'ALICE', cenb, idf)
-#               for (s, _) in pi_K_p:
-#                       entry['Pb-Pb-2760']['dN_dy'][s][:,idf] = info['obs'][s]
-#               if plot:
-#                       plt.subplot(2,4,3)
-#                       for (s, _) in pi_K_p:
-#                               #plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], fmt='o-', label=s)
-#                               plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], label=s)
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$dN/dy$', fontsize=7)
-#                       plt.legend()
-#
-#               # dN(pid)/dy exotic
-#
-#               cenb = np.array([[0,10],[10,20],[20,40],[40,60]])
-#               info = calculate_dNdy(res, 'ALICE', cenb, idf)
-#               for s in ['Lambda', 'Omega','Xi']:
-#                       entry['Pb-Pb-2760']['dN_dy-s'][s][:,idf] = info['obs'][s]
-#               if plot:
-#                       plt.subplot(2,4,4)
-#                       for (s, _) in lambda_omega_xi:
-#                               #plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], fmt='o-', label=s)
-#                               plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], label=s)
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$dN/dy$', fontsize=7)
-#                       plt.legend()
-#
-#
-#               # mean-pT
-#               cenb = np.array([[0,5],[5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70]])
-#               info = calculate_mean_pT(res, 'ALICE', cenb, idf)
-#               for (s, _) in pi_K_p:
-#                       entry['Pb-Pb-2760']['mean_pT'][s][:,idf] = info['obs'][s]
-#               if plot:
-#                       plt.subplot(2,4,5)
-#                       for (s, _) in pi_K_p:
-#                               #plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], fmt='o-', label=s)
-#                               plt.errorbar(info['cenM'], info['obs'][s], yerr=info['err'][s], label=s)
-#                       plt.ylim(0,2)
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$\langle p_T\rangle$', fontsize=7)
-#                       plt.legend()
-#
-#               # mean-pT-fluct
-#               cenb = np.array([[0,5],[5,10],[10,15],[15,20],[20,25],[25,30],[30,35],[35,40],[40,45],[45,50],[50,55],[55,60]])
-#               info = calculate_mean_pT_fluct(res, 'ALICE', cenb, idf)
-#               entry['Pb-Pb-2760']['pT_fluct'][:,idf] = info['obs']
-#               if plot:
-#                       plt.subplot(2,4,6)
-#                       #plt.errorbar(info['cenM'], info['obs'], yerr=info['err'], fmt='o-')
-#                       plt.errorbar(info['cenM'], info['obs'], yerr=info['err'])
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$\delta p_T/p_T$', fontsize=7)
-#                       #plt.legend()
-#
-#               # vn
-#               cenb = np.array([[0,5],[5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70]])
-#               #cenb = np.array([[0,10],[20,30],[30,40],[40,50],[50,60],[60,70],[70,80]])
-#               info = calculate_vn(res, 'ALICE', cenb, idf)
-#               entry['Pb-Pb-2760']['v22'][:,idf] = info['obs'][:, 1]
-#               entry['Pb-Pb-2760']['v32'][:,idf]  = info['obs'][:6, 2]
-#               entry['Pb-Pb-2760']['v42'][:,idf]  = info['obs'][:6, 3]
-#               if plot:
-#                       plt.subplot(2,4,7)
-#                       for n, c in zip([1,2,3],'rgb'):
-#                               #plt.errorbar(info['cenM'], info['obs'][:,n], yerr=info['err'][:,n], fmt=c+'o')
-#                               plt.errorbar(info['cenM'], info['obs'][:,n], yerr=info['err'][:,n], label=str(n+1))
-#                       plt.xlabel(r'Centrality (%)', fontsize=7)
-#                       plt.ylabel(r'$v_n$', fontsize=7)
-#                       plt.ylim(0,.15)
-#                       plt.legend()
-#
-#               # vn-diff
-#               """
-#               cenb = np.array([[30,40]])
-#               pTbins = np.array([[0,.2], [.2,.4], [.4,.6],[.6,.8],[.8,1.],
-#                               [1.,1.2], [1.2,1.5], [1.5,2.], [2.,2.5], [2.5,3]])
-#               info = calculate_diff_vn(res, 'ALICE', cenb, pTbins, idf, pid='chg')
-#               entry['Pb-Pb-2760']['v22-d'][:,idf] = info['obs'][:, :, 1]
-#               entry['Pb-Pb-2760']['v32-d'][:,idf]  = info['obs'][:, :, 2]
-#               entry['Pb-Pb-2760']['v42-d'][:,idf]  = info['obs'][:, :, 3]
-#               if plot:
-#                       plt.subplot(2,4,7)
-#                       for n in np.arange(1,4):
-#                               plt.errorbar(info['pTM'], info['obs'][0,:,n], yerr=info['err'][0,:,n], fmt='o-', label=r"$v_{:d}$".format(n+1))
-#                       plt.ylim(0,.3)
-#                       plt.xlabel(r'$p_T$ [GeV]', fontsize=7)
-#                       plt.ylabel(r'charged $v_{:d}$'.format(n+1), fontsize=7)
-#                       plt.legend()
-#               """
 
 
 
 if __name__ == '__main__':
         results = []
         for file in glob.glob(sys.argv[1]):
+                # Load calculations       
                 calcs = np.fromfile(file, dtype=np.dtype(bayes_dtype))
+
+                # Count how many observables to plot
                 nb_obs=len(final_obs_grouping)
+                # Decide how many columns we want the plot to have
                 nb_of_cols=4
+                # COunt how many rows needed
                 nb_of_rows=int(np.ceil(nb_obs/nb_of_cols))
+                # Prepare figure
                 fig = plt.figure(figsize=(2*nb_of_cols,2*nb_of_rows))
+
                 entry = plot(calcs)
