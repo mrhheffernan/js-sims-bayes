73be540718eb8fea224dba0ae8052db40727432d
Jean-francois Paquet <jp401@grads-20.internal.phy.duke.edu>
2019-09-06 16:25:04 -0400

    Don't try to fix NaN's, see if you can live with them

--- src/bayes_model.py
+++ src/bayes_model.py
@@ -14,9 +14,9 @@ for pt in range(n_design_pts_main): # loop over all design points
     for obs in active_obs_list[system_str]:
         values = np.array( model_data[system_str][pt, idf][obs]['mean'])
         # delete Nan dataset
-        isnan = np.isnan(values)
-        if np.sum(isnan) > 0:
-            model_data[system_str][pt, idf][obs]['mean'][isnan] = np.mean(values[np.logical_not(isnan)])
+        #isnan = np.isnan(values)
+        #if np.sum(isnan) > 0:
+        #    model_data[system_str][pt, idf][obs]['mean'][isnan] = np.mean(values[np.logical_not(isnan)])
 
         #transforming yield related observables
         #is_mult = ('dN' in obs) or ('dET' in obs)
