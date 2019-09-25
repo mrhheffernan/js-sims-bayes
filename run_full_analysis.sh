56a9e938569a6cf4ee47716bfeda36e4df3f8f43
Derek Everett <derekeverett@gmail.com>
2019-09-25 10:53:24 -0400

    add PCA diagnostic plot to workflow

--- run_full_analysis.sh
+++ run_full_analysis.sh
@@ -10,11 +10,15 @@ rm mcmc/chain.hdf
 #perform MCMC
 ./src/bayes_mcmc.py 4000 --nwalkers 100 --nburnsteps 500
 
-#generating plots of posteriors 
-./src/bayes_plot.py plots/diag_posterior.png
-./src/bayes_plot.py plots/viscous_posterior.png
-./src/bayes_plot.py plots/obs_validation.png
 
+./src/bayes_plot.py plots/diag_pca.png # PCA Diagnostic
+./src/bayes_plot.py plots/obs_prior.png # The prior against expt data
+./src/bayes_plot.py plots/diag_posterior.png # The posterior of parameters
+./src/bayes_plot.py plots/viscous_posterior.png # the posterior of eta/s and zeta/s
+./src/bayes_plot.py plots/obs_validation.png # Samples from the emulator using MAP against experimental data (the fit)
+
+open plots/diag_pca.png
+open plots/obs_prior.png
 open plots/diag_posterior.png
 open plots/viscous_posterior.png
 open plots/obs_validation.png
