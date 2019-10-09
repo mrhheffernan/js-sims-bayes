e6551880be064592b9ba7ad5f9fc31bfbb658187
Derek Everett <derekeverett@gmail.com>
2019-10-09 15:33:12 -0400

    updates to script to run closure test in parallel

--- run_closure_test_parallel.sh
+++ run_closure_test_parallel.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/bash
 
-declare -a bgpids
-
-cleanup() {
-    for pid in ${bgpids[@]}; do
-        kill -9 $pid
-    done
-}
-
-trap "cleanup" SIGINT SIGTERM
-
 cd closure_parallel
 
 for i in {0..99}; do
@@ -29,9 +19,10 @@ for i in {0..99}; do
   mkdir plots
   mkdir closure_truth_dob
 
-  #sed -i .bak "s/validation_pt=.*/validation_pt=$i/g" src/configurations.py
+  #change the validation point
+  sed -i .bak "s/validation_pt=.*/validation_pt=$i/g" src/configurations.py
   #perform MCMC
-  ./src/bayes_mcmc.py 3000 --nwalkers 70 --nburnsteps 1000
+  ./src/bayes_mcmc.py 2000 --nwalkers 50 --nburnsteps 1000
 
   #generate plots
   ./src/bayes_plot.py plots/diag_posterior.png
@@ -40,7 +31,6 @@ for i in {0..99}; do
   ./src/emulator_load_and_validate.py
   cd ..
   } &
-  bgpids+=("$!")
 done
 
 #wait til all processes have finished
