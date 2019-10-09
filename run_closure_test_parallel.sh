fb4a64413000d58f26c3c981fbef1a05ff189cbf
Derek Everett <derekeverett@gmail.com>
2019-10-08 21:07:51 -0400

    updates to run closure test in parallel on TACC

--- run_closure_test_parallel.sh
+++ run_closure_test_parallel.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/bash
 
+declare -a bgpids
+
+cleanup() {
+    for pid in ${bgpids[@]}; do
+        kill -9 $pid
+    done
+}
+
+trap "cleanup" SIGINT SIGTERM
+
+cd closure_parallel
+
 for i in {0..99}; do
   {
-  mkdir closure_parallel/$i
-  cd closure_parallel/$i
+  mkdir $i
+  cd $i
+  pwd
 
   cp -r ../../src .
   cp -r ../../HIC_experimental_data .
-  cp -r ../../closure_truth_dob .
   cp -r ../../design_pts .
   cp -r ../../model_calculations .
   cp -r ../../emulator .
-  cp -r ../../mcmc .
 
-  sed -i .bak "s/validation_pt=.*/validation_pt=$i/g" src/configurations.py
+  mkdir mcmc
+  mkdir closure_plots
+  mkdir plots
+  mkdir closure_truth_dob
 
-  #remove existing mcmc chains
-  rm mcmc/chain.hdf
-
+  #sed -i .bak "s/validation_pt=.*/validation_pt=$i/g" src/configurations.py
   #perform MCMC
-  ./src/bayes_mcmc.py 4000 --nwalkers 100 --nburnsteps 1000
+  ./src/bayes_mcmc.py 3000 --nwalkers 70 --nburnsteps 1000
 
   #generate plots
-  #./src/bayes_plot.py plots/diag_posterior.png
-  #cp plots/diag_posterior.png closure_plots/diag_posterior_$i.png
+  ./src/bayes_plot.py plots/diag_posterior.png
 
   #generate file of truth and credibility
   ./src/emulator_load_and_validate.py
+  cd ..
   } &
-
+  bgpids+=("$!")
 done
 
 #wait til all processes have finished
