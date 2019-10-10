a7cdd7446ddf831298c42350671455223ac72210
Jean-francois Paquet <jp401@grads-20.internal.phy.duke.edu>
2019-10-10 19:20:50 -0400

    Update script to extract confidence intervals

--- src/CR.py
+++ src/CR.py
@@ -5,64 +5,69 @@ import numpy as np
 from bayes_mcmc import Chain
 import pandas as pd
 import matplotlib.pyplot as plt
-#get model calculations at VALIDATION POINTS
-logging.info("Load calculations from " + f_obs_validation)
-Yexp_PseudoData_pre = np.fromfile(f_obs_validation, dtype=bayes_dtype)
-Yexp_PseudoData=Yexp_PseudoData_pre[validation_pt]
 
-
-design_file = design_dir + \
-       '/design_points_validation_{:s}{:s}-{:d}.dat'.format(*systems[0])
-logging.info("Loading design points from " + design_file)
-design = pd.read_csv(design_file)
-design = design.drop("idx", axis=1)
-truth = design.values[validation_pt]
+# load validation points
+system = systems[0]
+design, design_min, design_max, labels = load_design(system, pset = 'validation')
+truth = design.values[validation_pt] # true parameter  of a point
 
 chain = Chain()
 data = chain.load().T[:-1]
 ndims, nsamples = data.shape
 
-def zetas(T, zmax, width, T0, asym):
-    DeltaT = T - T0
-    sign = 1 if DeltaT>0 else -1
-    x = DeltaT/(width*(1.+asym*sign))
-    return zmax/(1.+x**2) 
-zetas = np.vectorize(zetas)
+# Take samples from the chain
+n_sample_from_chain=50000
+sample_id_from_chain=np.random.choice(np.arange(nsamples), size=n_sample_from_chain, replace=False)
+X=data[:,sample_id_from_chain].T
 
-with open("validate/{:d}.dat".format(validation_pt),'w') as f:
+with open("validate/{:d}-original.dat".format(validation_pt),'w') as f:
+    # Open chain, and then
+    # writes the credible limit of the original parameters
+    f.write("# index True, Median, low-5%, low-20%, high-80%, high-95%\n")
     for i in range(ndims):
-        samples = data[i]
-        H, xbins = np.histogram(samples, bins=21)
-        x = (xbins[1:] + xbins[:-1])/2.
-        m = np.median(samples)
-        M = x[np.argmax(H)]
-        l1 = np.quantile(samples, .2)
-        l2 = np.quantile(samples, .1)
-        h1 = np.quantile(samples, .8)
-        h2 = np.quantile(samples, .9)
-        f.write("{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\n".format(
-                 truth[i], m, M, l1, l2, h1, h2
+        samples = X[i]
+        median = np.median(samples)
+        l5 = np.quantile(samples, .05)
+        l20 = np.quantile(samples, .2)
+        h80 = np.quantile(samples, .8)
+        h95 = np.quantile(samples, .95)
+        f.write("{:d}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\n".format(
+                 i, truth[i], median, l5, l20, h80, h95
                  )
                )
 
-    for Ttest in [.155, .175, .2, .25, .35]:
-        width=2./np.pi*data[-2]**4/data[-4]
-        samples = zetas(Ttest, data[-4], width, data[-3], data[-1])
-        H, xbins = np.histogram(samples, bins=21)
-        x = (xbins[1:] + xbins[:-1])/2.
-        m = np.median(samples)
-        M = x[np.argmax(H)]
-
-        l1 = np.quantile(samples, .2)
-        l2 = np.quantile(samples, .1)
-        h1 = np.quantile(samples, .8)
-        h2 = np.quantile(samples, .9)
-
-        width=2./np.pi*truth[-2]**4/truth[-4]
-        tt = zetas(Ttest, truth[-4], width, truth[-3], truth[-1])
-
+with open("validate/{:d}-etas.dat".format(validation_pt),'w') as f:
+    # transform design into eta/s(T_i) and zeta/s(T_i)
+    # Ti is chose, e.g, to be
+    #Ti = [.155, .175, .2, .25, .35]
+    Ti = np.linspace(.15,.35, num=50)
+    f.write("# T, Median, low-5%, low-20%, high-80%, high-95%\n")
+    for T in Ti:
+        samples = eta_over_s(T, X[:, 7], X[:, 8], X[:, 9], X[:, 10])
+        median = np.median(samples)
+        l5 = np.quantile(samples, .05)
+        l20 = np.quantile(samples, .2)
+        h80 = np.quantile(samples, .8)
+        h95 = np.quantile(samples, .95)
         f.write("{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\n".format(
-                 tt, m, M, l1, l2, h1, h2
+                 T, truth[i], median, l5, l20, h80, h95
                  )
                )
 
+with open("validate/{:d}-zetas.dat".format(validation_pt),'w') as f:
+    # transform design into eta/s(T_i) and zeta/s(T_i)
+    # Ti is chose, e.g, to be
+    #Ti = [.155, .175, .2, .25, .35]
+    Ti = np.linspace(.15,.35, num=50)
+    f.write("# T, Median, low-5%, low-20%, high-80%, high-95%\n")
+    for T in Ti:
+        samples = zeta_over_s(T, X[:, 11], X[:, 12], X[:, 13], X[:, 14])
+        median = np.median(samples)
+        l5 = np.quantile(samples, .05)
+        l20 = np.quantile(samples, .2)
+        h80 = np.quantile(samples, .8)
+        h95 = np.quantile(samples, .95)
+        f.write("{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\t{:1.6f}\n".format(
+                 T, truth[i], median, l5, l20, h80, h95
+                 )
+               )
