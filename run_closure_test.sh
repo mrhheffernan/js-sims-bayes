6426bb2609a0766ed7b80fc889362085abcc9e4c
Derek Everett <derekeverett@gmail.com>
2019-10-11 16:58:48 -0400

    renaming some files with more descriptive names

--- run_closure_test.sh
+++ run_closure_test.sh
@@ -4,5 +4,5 @@ for i in {0..99}; do
     sed -i "s/validation=.*/validation=$i/g" src/configurations.py
     rm -f mcmc/*
     ./src/bayes_mcmc.py 2000 --nwalkers 100 --nburnsteps 500 || exit
-    ./src/CR.py || exit
+    ./src/emulator_truth_vs_dob.py || exit
 done
