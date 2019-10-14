199c33737b8e27bfb5afaa0e1812e0d41bd8bba9
Derek Everett <derekeverett@gmail.com>
2019-10-14 10:55:01 -0400

    added observables_fit plot for compact view of observables fit to experiment

--- src/bayes_plot.py
+++ src/bayes_plot.py
@@ -29,6 +29,7 @@ import warnings
 import h5py
 import hsluv
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import lines
 from matplotlib import patches
@@ -375,12 +376,36 @@ def observables_fit():
     """
     print("Plotting observables drawn from posterior")
 
+    font_size = 12
+
     obs_groups = {
                 'yields' : ['dNch_deta', 'dET_deta', 'dN_dy_pion', 'dN_dy_kaon', 'dN_dy_proton'],
                 'mean_pT' : ['mean_pT_pion', 'mean_pT_kaon', 'mean_pT_proton'],
-                'fluct' : ['mean_pT_fluct'],
+                'fluct' : ['pT_fluct'],
                 'flows' : ['v22', 'v32', 'v42']
                 }
+    obs_group_labels = {
+                'yields' : r'$dN_{ch}/d\eta$ , $dN/dy$, $dE_T/d\eta$ [GeV]',
+                'mean_pT' : r'$ \langle p_T \rangle$',
+                'fluct' : r'$\delta p_T / \langle p_T \rangle$',
+                'flows' : r'$v_n \{ 2 \} $'
+                }
+    obs_tex_labels = {
+                        'dNch_deta' : r'$dN_{ch}/d\eta$',
+                        'dN_dy_pion' : r'$dN_{\pi}/dy$',
+                        'dN_dy_kaon' : r'$dN_{k}/dy$',
+                        'dN_dy_proton' : r'$dN_{p}/dy$',
+                        'dET_deta' : r'$dE_{T}/d\eta$',
+                        'mean_pT_pion' : r'$\pi$',
+                        'mean_pT_kaon' : r'$k$',
+                        'mean_pT_proton' : r'$p$',
+                        'pT_fluct' : None,
+                        'v22' : r'$v_2\{2\}$',
+                        'v32' : r'$v_3\{2\}$',
+                        'v42' : r'$v_4\{2\}$',
+
+    }
+    colors = ['b', 'g', 'r', 'c', 'm']
     #is this what we want, what exactly does Chain().samples() return ???
     Ymodel = Chain().samples(100)
     Yexp = Y_exp_data
@@ -388,29 +413,52 @@ def observables_fit():
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
     for system in system_strs:
         for ax, obs_group in zip(axes.flatten(), obs_groups.keys() ):
-            for obs in obs_group:
+            for obs, color in zip(obs_groups[obs_group], colors):
+                if obs_group == 'yields':
+                    ax.set_yscale('log')
+                scale = 1.0
+                if obs == 'dET_deta':
+                    scale = 20.
+                if obs == 'dNch_deta':
+                    scale = 5.
+                ax.set_ylabel(obs_group_labels[obs_group], fontsize=font_size)
+
                 xbins = np.array(obs_cent_list[system][obs])
                 x = (xbins[:,0]+xbins[:,1])/2.
                 Y = Ymodel[system][obs]
                 for iy, y in enumerate(Y):
-                    lw=0.3
-                    alpha=0.2
                     is_mult = ('dN' in obs) or ('dET' in obs)
                     if is_mult and transform_multiplicities:
                         y = np.exp(y) - 1.0
-
-                    ax.plot(x, y, alpha=alpha, lw=lw, label=obs)
+                    label = None
+                    if iy == 0:
+                        label=obs_tex_labels[obs]
+                    ax.plot(x, y*scale, alpha=0.3, lw=0.3, color=color, label=label)
                 try:
                     exp_mean = Yexp[system][obs]['mean'][:, 0][0]
                     exp_err = Yexp[system][obs]['err'][:, 0][0]
                 except KeyError:
                     continue
-                if obs_group == 'yields':
-                    ax.set_yscale('log')
-                ax.errorbar( x, exp_mean, exp_err, fmt='o', color='black')
+
+                ax.errorbar( x, exp_mean*scale, exp_err, fmt='o', color='black')
+
+                leg = ax.legend(fontsize=font_size)
+                for legobj in leg.legendHandles:
+                    legobj.set_linewidth(2.0)
+                    legobj.set_alpha(1.0)
+
+                ax.set_xlim(0, 80)
+                if obs_group == 'mean_pT':
+                    ax.set_ylim(0.0, 1.5)
+                if obs_group == 'fluct':
+                    ax.set_ylim(0.0, 0.04)
+                if obs_group == 'flows':
+                    ax.set_ylim(0.0, 0.12)
+                #ax.text(2,4,'This text starts at point (2,4)')
+
 
             if ax.is_last_row():
-                ax.set_xlabel('Centrality %')
+                ax.set_xlabel('Centrality %', fontsize=font_size)
 
     set_tight(fig, rect=[0, 0, .97, 1])
 
