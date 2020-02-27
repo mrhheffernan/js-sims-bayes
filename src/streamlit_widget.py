6e8f7e0cc9a41d2c815411752dc0bf60a3de7315
Derek Everett <derekeverett@gmail.com>
2020-02-27 17:39:32 -0500

    putting inside functions, playing with caching. Emu is fast, why is plotting slow?

--- src/streamlit_widget.py
+++ src/streamlit_widget.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
 import numpy as np
+import time
 import matplotlib
 import dill
 import matplotlib.pyplot as plt
@@ -39,76 +39,117 @@ system_observables = {
 system = 'Pb-Pb-2760'
 Yexp = Y_exp_data
 
-#load the design
-design_file = SystemsInfo[system]["main_design_file"]
-range_file = SystemsInfo[system]["main_range_file"]
-design = pd.read_csv(design_file)
-design = design.drop("idx", axis=1)
-labels = design.keys()
-design_range = pd.read_csv(range_file)
-design_max = design_range['max'].values
-design_min = design_range['min'].values
-Names = labels
+@st.cache(persist=True)
+def load_design(system):
+    #load the design
+    design_file = SystemsInfo[system]["main_design_file"]
+    range_file = SystemsInfo[system]["main_range_file"]
+    design = pd.read_csv(design_file)
+    design = design.drop("idx", axis=1)
+    labels = design.keys()
+    design_range = pd.read_csv(range_file)
+    design_max = design_range['max'].values
+    design_min = design_range['min'].values
+
+    return design, labels, design_max, design_min
+
+
+@st.cache(allow_output_mutation=True)
+def load_emu(system, idf):
+    #load the emulator
+    emu = dill.load(open('emulator/emulator-' + system + '-idf-' + str(idf) + '.dill', "rb"))
+
+    return emu
+
+
+@st.cache(persist=True)
+def load_obs(system):
+    observables = system_observables[system]
+    nobs = len(observables)
+
+    return observables, nobs
+
+
+@st.cache(persist=True)
+def emu_predict(params):
+    start = time.time()
+    Yemu_cov = 0
+    #Yemu_mean, Yemu_cov = emu.predict( np.array( [params] ), return_cov=True )
+    Yemu_mean = emu.predict( np.array( [params] ), return_cov=False )
+    end = time.time()
+    time_emu = end - start
+    return Yemu_mean, Yemu_cov, time_emu
+
+#@st.cache()
+def make_plot(Yemu_mean, Yemu_cov):
+    nrows = 3
+    fig, axes = plt.subplots(nrows=nrows, ncols= nobs // nrows, figsize=(6,6))
+
+    for obs, ax in zip(observables, axes.flatten()):
+        ax.cla()
+        ax.set_title(obs_tex_labels_2[obs])
+        xbins = np.array(obs_cent_list[system][obs])
+        #centrality bins
+        x = (xbins[:,0]+xbins[:,1])/2.
+        #emulator prediction
+        y_emu = Yemu_mean[obs][0]
+
+        #dy_emu = (np.diagonal(np.abs(Yemu_cov[obs, obs]))**.5)[:,0]
+
+        #FAKE ERROR BAR FOR TESTING
+        dy_emu = y_emu * 0.1
+
+        ax.fill_between(x, y_emu-dy_emu, y_emu+dy_emu)
+        #experiment
+        exp_mean = Yexp[system][obs]['mean'][idf]
+        exp_err = Yexp[system][obs]['err'][idf]
+        ax.errorbar( x, exp_mean, exp_err, color='black', marker='v')
+
+        if obs == 'dNch_deta':
+            ax.set_ylim(0, 2e3)
+        if obs == 'dN_dy_pion':
+            ax.set_ylim(0, 2e3)
+        if obs == 'dN_dy_proton':
+            ax.set_ylim(0, 1e2)
+        if obs == 'dET_deta':
+            ax.set_ylim(0, 2.5e3)
+        if obs == 'mean_pT_pion':
+            ax.set_ylim(0.3, 0.7)
+        if obs == 'mean_pT_proton':
+            ax.set_ylim(0.75, 1.75)
+        if obs == 'pT_fluct':
+            ax.set_ylim(0, 0.04)
+        if obs == 'v22':
+            ax.set_ylim(0, 0.13)
+        if obs == 'v32':
+            ax.set_ylim(0.01, 0.04)
+        if obs == 'v42':
+            ax.set_ylim(0.005, 0.018)
+
+    plt.tight_layout(True)
+    st.pyplot(fig)
+
+
+system = 'Pb-Pb-2760'
 idf = 0
 
-#load the emulator
-emu = dill.load(open('emulator/emulator-' + 'Pb-Pb-2760' + '-idf-' + str(idf) + '.dill', "rb"))
+#load the design
+design, labels, design_max, design_min = load_design(system)
 
+#load the emu
+emu = load_emu(system, idf)
 
-observables = system_observables[system]
-nobs = len(observables)
-nrows = 3
-fig, axes = plt.subplots(nrows=nrows, ncols= nobs // nrows, figsize=(6,6))
+#load the exp obs
+observables, nobs = load_obs(system)
 
-
-#update the parameters with slider HERE
-#params = [getattr(self, 'tune'+name).get() for name in Names]
-
-#params = MAP_params[system]
-
-
+#get emu prediction
 params = [14.128, 0.089, 1.054, 1.064, 4.227, 1.507, 0.113, 0.223, -1.585, 0.32, 0.056, 0.11, 0.16, 0.093, -0.084, 4.666, 0.136]
-Yemu_mean, Yemu_cov = emu.predict( np.array( [params] ), return_cov=True )
+Yemu_mean, Yemu_cov, time_emu = emu_predict(params)
 
-for obs, ax in zip(observables, axes.flatten()):
-    ax.cla()
-    ax.set_title(obs_tex_labels_2[obs])
-    xbins = np.array(obs_cent_list[system][obs])
-    #centrality bins
-    x = (xbins[:,0]+xbins[:,1])/2.
-    #emulator prediction
-    y_emu = Yemu_mean[obs][0]
-    dy_emu = (np.diagonal(np.abs(Yemu_cov[obs, obs]))**.5)[:,0]
-    ax.fill_between(x, y_emu-dy_emu, y_emu+dy_emu)
-    #experiment
-    exp_mean = Yexp[system][obs]['mean'][idf]
-    exp_err = Yexp[system][obs]['err'][idf]
-    ax.errorbar( x, exp_mean, exp_err, color='black', marker='v')
-
-    if obs == 'dNch_deta':
-        ax.set_ylim(0, 2e3)
-    if obs == 'dN_dy_pion':
-        ax.set_ylim(0, 2e3)
-    if obs == 'dN_dy_proton':
-        ax.set_ylim(0, 1e2)
-    if obs == 'dET_deta':
-        ax.set_ylim(0, 2.5e3)
-    if obs == 'mean_pT_pion':
-        ax.set_ylim(0.3, 0.7)
-    if obs == 'mean_pT_proton':
-        ax.set_ylim(0.75, 1.75)
-    if obs == 'pT_fluct':
-        ax.set_ylim(0, 0.04)
-    if obs == 'v22':
-        ax.set_ylim(0, 0.13)
-    if obs == 'v32':
-        ax.set_ylim(0.01, 0.04)
-    if obs == 'v42':
-        ax.set_ylim(0.005, 0.018)
-
-
-plt.tight_layout(True)
-st.pyplot(fig)
+#make plot
+make_plot(Yemu_mean, Yemu_cov)
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
+
+st.write('emu took ' + str(time_emu) + ' sec')
