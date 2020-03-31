9e0d76b3d84d48ac550e6760da923e723791a7fa
Derek Everett <derekeverett@gmail.com>
2020-03-31 17:50:28 -0400

    changed plot labels to plain text since latex/markdown is not capable in altair

--- src/streamlit_widget.py
+++ src/streamlit_widget.py
@@ -38,7 +38,27 @@ system_observables = {
 
 obs_lims = {'dET_deta' : 2000. , 'dN_dy_pion' : 2000., 'dN_dy_proton' : 100., 'mean_pT_pion' : 1., 'mean_pT_proton' : 2., 'pT_fluct' : .05, 'v22' : .2, 'v32' : .05, 'v42' :.03 }
 
+obs_word_labels = {
+                    'dNch_deta' : r'charged multiplicity',
+                    'dN_dy_pion' : r'pion yield',
+                    'dN_dy_kaon' : r'Kaon yield',
+                    'dN_dy_proton' : r'proton yield',
+                    'dN_dy_Lambda' : r'lambda yield',
+                    'dN_dy_Omega' : r'omega yield',
+                    'dN_dy_Xi' : r'xi yield',
+                    'dET_deta' : r'transv. energy [GeV]',
+                    'mean_pT_pion' : r'pion mean pT [GeV]',
+                    'mean_pT_kaon' : r'kaon mean pT [GeV]',
+                    'mean_pT_proton' : r'proton mean pT [GeV]',
+                    'pT_fluct' : r'mean pT fluct.',
+                    'v22' : r'ellip. flow',
+                    'v32' : r'triang. flow',
+                    'v42' : r'quad. flow',
+}
+
 system = 'Pb-Pb-2760'
+
+#alt.renderers.enable(notebook, embed_options={'renderer': 'svg'})
 
 @st.cache(persist=True)
 def load_design(system):
@@ -69,17 +89,17 @@ def load_obs(system):
     return observables, nobs, Yexp
 
 
-@st.cache()
+@st.cache(allow_output_mutation=True)
 def emu_predict(params):
     start = time.time()
     Yemu_cov = 0
-    Yemu_mean = emu.predict( np.array( [params] ), return_cov=False )
-    #Yemu_mean, Yemu_cov = emu.predict( np.array( [params] ), return_cov=True )
+    #Yemu_mean = emu.predict( np.array( [params] ), return_cov=False )
+    Yemu_mean, Yemu_cov = emu.predict( np.array( [params] ), return_cov=True )
     end = time.time()
     time_emu = end - start
     return Yemu_mean, Yemu_cov, time_emu
 
-#@st.cache()
+#@st.cache(suppress_st_warning=True)
 def make_plot_altair(Yemu_mean, Yemu_cov, Yexp, idf):
     for iobs, obs in enumerate(observables):
         xbins = np.array(obs_cent_list[system][obs])
@@ -88,11 +108,8 @@ def make_plot_altair(Yemu_mean, Yemu_cov, Yexp, idf):
         #emulator prediction
         y_emu = Yemu_mean[obs][0]
 
-        #FAKE ERROR BAR FOR TESTING
-        dy_emu = y_emu * 0.1
-        #dy_emu = (np.diagonal(np.abs(Yemu_cov[obs, obs]))**.5)[:,0]
+        dy_emu = (np.diagonal(np.abs(Yemu_cov[obs, obs]))**.5)[:,0]
         df_emu = pd.DataFrame({'cent': x, 'yl':y_emu - dy_emu, "yh":y_emu + dy_emu})
-
 
         chart_emu = alt.Chart(df_emu).mark_area().encode(x='cent', y='yl', y2='yh').properties(width=150,height=150)
 
@@ -103,7 +120,7 @@ def make_plot_altair(Yemu_mean, Yemu_cov, Yexp, idf):
 
         chart_exp = alt.Chart(df_exp).mark_circle(color='Black').encode(
         x=alt.X( 'cent', axis=alt.Axis(title='cent'), scale=alt.Scale(domain=(0, 70)) ),
-        y=alt.Y(obs, axis=alt.Axis(title=obs_tex_labels_2[obs]), scale=alt.Scale(domain=(0, obs_lims[obs]))  )
+        y=alt.Y(obs, axis=alt.Axis(title=obs_word_labels[obs]), scale=alt.Scale(domain=(0, obs_lims[obs]))  )
         )
 
         chart = alt.layer(chart_emu, chart_exp)
@@ -123,9 +140,26 @@ def make_plot_altair(Yemu_mean, Yemu_cov, Yexp, idf):
         if iobs in [7, 8]:
             charts2 = alt.hconcat(charts2, chart)
 
-    st.write(charts0)
-    st.write(charts1)
-    st.write(charts2)
+
+    charts0 = st.altair_chart(charts0)
+    charts1 = st.altair_chart(charts1)
+    charts2 = st.altair_chart(charts2)
+
+    #return charts0, charts1, charts2
+
+#@st.cache(suppress_st_warning=True)
+def update_plot_altair(Yemu_mean, Yemu_cov, Yexp, idf, charts0, charts1, charts2):
+    for iobs, obs in enumerate(observables):
+        xbins = np.array(obs_cent_list[system][obs])
+        #centrality bins
+        x = (xbins[:,0]+xbins[:,1])/2.
+        #emulator prediction
+        y_emu = Yemu_mean[obs][0]
+
+        dy_emu = (np.diagonal(np.abs(Yemu_cov[obs, obs]))**.5)[:,0]
+        df_emu = pd.DataFrame({'cent': x, 'yl':y_emu - dy_emu, "yh":y_emu + dy_emu})
+
+        charts0.add_rows(df_emu)
 
 
 system = 'Pb-Pb-2760'
@@ -148,7 +182,6 @@ params_0 = MAP_params[system][ idf_label_short[idf] ]
 #updated params
 params = []
 for i_s, s_name in enumerate(short_names.keys()):
-    #s = st.markdown(short_names[s_name])
     min = design_min[i_s]
     max = design_max[i_s]
     p = st.sidebar.slider(short_names[s_name], min_value=min, max_value=max, value=params_0[i_s])
