b36d7a0f21dcf8de0d5bb3a1d6d0ed3e5d9742f3
Jean-francois Paquet <jp401@grads-20.internal.phy.duke.edu>
2020-10-31 12:35:50 -0400

    Add greek letters to widget (for eta/s and zeta/s)

--- src/streamlit_widget.py
+++ src/streamlit_widget.py
@@ -12,6 +12,14 @@ from emulator import Trained_Emulators, _Covariance
 from bayes_exp import Y_exp_data
 from bayes_plot import obs_tex_labels_2
 
+# https://gist.github.com/beniwohli/765262
+greek_alphabet_inv = { u'\u0391': 'Alpha', u'\u0392': 'Beta', u'\u0393': 'Gamma', u'\u0394': 'Delta', u'\u0395': 'Epsilon', u'\u0396': 'Zeta', u'\u0397': 'Eta', u'\u0398': 'Theta', u'\u0399': 'Iota', u'\u039A': 'Kappa', u'\u039B': 'Lamda', u'\u039C': 'Mu', u'\u039D': 'Nu', u'\u039E': 'Xi', u'\u039F': 'Omicron', u'\u03A0': 'Pi', u'\u03A1': 'Rho', u'\u03A3': 'Sigma', u'\u03A4': 'Tau', u'\u03A5': 'Upsilon', u'\u03A6': 'Phi', u'\u03A7': 'Chi', u'\u03A8': 'Psi', u'\u03A9': 'Omega', u'\u03B1': 'alpha', u'\u03B2': 'beta', u'\u03B3': 'gamma', u'\u03B4': 'delta', u'\u03B5': 'epsilon', u'\u03B6': 'zeta', u'\u03B7': 'eta', u'\u03B8': 'theta', u'\u03B9': 'iota', u'\u03BA': 'kappa', u'\u03BB': 'lamda', u'\u03BC': 'mu', u'\u03BD': 'nu', u'\u03BE': 'xi', u'\u03BF': 'omicron', u'\u03C0': 'pi', u'\u03C1': 'rho', u'\u03C3': 'sigma', u'\u03C4': 'tau', u'\u03C5': 'upsilon', u'\u03C6': 'phi', u'\u03C7': 'chi', u'\u03C8': 'psi', u'\u03C9': 'omega', }
+greek_alphabet = {v: k for k, v in greek_alphabet_inv.items()}
+
+zeta_over_s_str=greek_alphabet['zeta']+'/s(T)'
+eta_over_s_str=greek_alphabet['eta']+'/s(T)'
+
+
 short_names = {
                 'norm' : r'Energy Normalization', #0
                 'trento_p' : r'TRENTo Reduced Thickness', #1
@@ -169,15 +177,15 @@ def make_plot_eta_zeta(params):
 
     df_eta_zeta = pd.DataFrame({'T': T, 'eta':eta_s, 'zeta':zeta_s})
 
-    chart_eta = alt.Chart(df_eta_zeta).mark_line().encode(
+    chart_eta = alt.Chart(df_eta_zeta, title='Specific shear viscosity').mark_line().encode(
     x=alt.X('T', axis=alt.Axis(title='T [GeV]'), scale=alt.Scale(domain=(T_low, T_high)) ),
-    y=alt.Y('eta', axis=alt.Axis(title='specific shear viscosity '), scale=alt.Scale(domain=(0., 0.5 ))  ),
+    y=alt.Y('eta', axis=alt.Axis(title=eta_over_s_str), scale=alt.Scale(domain=(0., 0.5 ))  ),
     color=alt.value("#FF0000")
     ).properties(width=150,height=150)
 
-    chart_zeta = alt.Chart(df_eta_zeta).mark_line().encode(
+    chart_zeta = alt.Chart(df_eta_zeta, title='Specific bulk viscosity').mark_line().encode(
     x=alt.X('T', axis=alt.Axis(title='T [GeV]'), scale=alt.Scale(domain=(T_low, T_high)) ),
-    y=alt.Y('zeta', axis=alt.Axis(title='specific bulk viscosity'), scale=alt.Scale(domain=(0., 0.5 ))  ),
+    y=alt.Y('zeta', axis=alt.Axis(title=zeta_over_s_str), scale=alt.Scale(domain=(0., 0.5 ))  ),
     color=alt.value("#FF0000")
     ).properties(width=150,height=150)
 
